1379205d70424ce47a61187df0e675e7335123ec
AkshayRaman97 <akshay.raman97@gmail.com>
2018-07-17 22:34:13 +0530

    changed background

--- _harp/js/index.js
+++ _harp/js/index.js
@@ -1,9 +1,11 @@
 $( "#btn-know-more" ).bind( "click", function() {
     $("#welcome-container").fadeOut( 300 );
+    $("canvas#intro-canvas").css('display','none');
     $("#about-container").delay(500).fadeIn();
 });
 
 $( "#btn-back" ).bind( "click", function() {
     $("#about-container").fadeOut( 300 );
+    $("canvas#intro-canvas").css('display','block');
     $("#welcome-container").delay(500).fadeIn();
 });
