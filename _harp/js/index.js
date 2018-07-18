103096d770ce162901edd718ff417ebd9671aa95
AkshayRaman97 <akshay.raman97@gmail.com>
2018-07-18 22:34:01 +0530

    Added cards

--- _harp/js/index.js
+++ _harp/js/index.js
@@ -1,11 +1,11 @@
 $( "#btn-know-more" ).bind( "click", function() {
     $("#welcome-container").fadeOut( 300 );
-    $("canvas#intro-canvas").css('display','none');
+    $("canvas#intro-canvas").toggleClass("faded");
     $("#about-container").delay(500).fadeIn();
 });
 
 $( "#btn-back" ).bind( "click", function() {
     $("#about-container").fadeOut( 300 );
-    $("canvas#intro-canvas").css('display','block');
+    $("canvas#intro-canvas").toggleClass("faded");
     $("#welcome-container").delay(500).fadeIn();
 });
