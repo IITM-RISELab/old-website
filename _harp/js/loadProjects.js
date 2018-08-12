901db7b85e8c33a0d4a5cd4ef049e0d15db09f8b
AkshayRaman97 <akshay.raman97@gmail.com>
2018-08-12 22:26:00 +0530

    changed projects page layout

--- _harp/js/loadProjects.js
+++ _harp/js/loadProjects.js
@@ -1,40 +1,42 @@
 // Initialize Firebase
 var config = {
-apiKey: "AIzaSyCvT-MHFirRHd3ccQZ4J9w1nGGy-PqQ8oM",
-authDomain: "rise-labs-website.firebaseapp.com",
-databaseURL: "https://rise-labs-website.firebaseio.com",
-projectId: "rise-labs-website",
-storageBucket: "rise-labs-website.appspot.com",
-messagingSenderId: "368224713702"
+    apiKey: "AIzaSyCvT-MHFirRHd3ccQZ4J9w1nGGy-PqQ8oM",
+    authDomain: "rise-labs-website.firebaseapp.com",
+    databaseURL: "https://rise-labs-website.firebaseio.com",
+    projectId: "rise-labs-website",
+    storageBucket: "rise-labs-website.appspot.com",
+    messagingSenderId: "368224713702"
 };
 firebase.initializeApp(config);
 var database = firebase.database()
 var databaseRef = database.ref('/projects')
 
 // Database Listener
-databaseRef.once("value", function(snapshot) {
+databaseRef.once("value", function (snapshot) {
     var projectList = snapshot.val();
     console.log(projectList)
     var keys = Object.keys(projectList)
-    for(var i=0;i<keys.length;i++){
+    for (var i = 0; i < keys.length; i++) {
         console.log(keys[i])
         var key = keys[i]
-        addProjectToHTML(projectList[key],key)
+        addProjectToHTML(projectList[key], key)
     }
     $('#loader-container').fadeOut().remove()
     $("#projects-container").fadeIn();
 });
 
-function addProjectToHTML(project,id){
+function addProjectToHTML(project, id) {
     var html = $([
+        '<div class="card-container center col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">',
         '<div class="card mx-auto">',
-            '<div class="card-body">',
-                '<h2 class="center">'+ project.title +'</h2>',
-                '<h4 class="center"><span style="color:#38b538" >Incharge : </span>' + project.faculty + '</h4>',
-                '<h4 class="center"><span style="color:#38b538" >Status : </span>' + project.status + '</h4>',
-                '<a class="btn btn-primary center" href="project.html?id=' + id + '" role="button">Learn more</a>',
-            '</div>',
+        '<div class="card-body">',
+        '<h2 class="center">' + project.title + '</h2>',
+        '<h4 class="center"><span style="color:#38b538" >Incharge : </span>' + project.faculty + '</h4>',
+        '<h4 class="center"><span style="color:#38b538" >Status : </span>' + project.status + '</h4>',
+        '<a class="btn btn-primary center" href="project.html?id=' + id + '" role="button">Learn more</a>',
         '</div>',
+        '</div>',
+        '</div>'
     ].join("\n"))
     $("#projects-list").prepend(html)
 }
